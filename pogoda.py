56ab032c47344e8d843419e8ff0f61019e3219d6
tsabur@ukr.net <tsabur@ukr.net>
2020-08-27 00:35:14 +0200

    initial commit

--- pogoda.py
+++ pogoda.py
@@ -10,3 +10,10 @@ temperature = w.get_temperature('celsius')['temp']
 print('В городе ' + city + ' сейчас: ' + str(temperature) + '°С')
 print('Также в городе ' + city + ' сейчас ' + w.get_detailed_status())
 
+# Выводит на экран погоду (облачно, дожть...) каждые три часа
+# fc1 = owm .three_hours_forecast ( city )
+# f = fc1.get_forecast()
+# for weather in f:
+#      print (weather.get_reference_time('iso'), weather.get_status(), weather.get_temperature(unit='celsius'))
+
+# Средняя температура - weather.get_temperature(unit='celsius')['temp']
